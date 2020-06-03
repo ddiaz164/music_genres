e1a9505027b165350524c0b9e13fc57a50a0d028
David Diaz <daviddiaz10.19@gmail.com>
2020-06-03 09:52:27 -0700

    day 3 prog

--- src/helper_funcs.py
+++ src/helper_funcs.py
@@ -8,15 +8,6 @@ from os.path import isfile
 import ast
 import librosa
 import librosa.display
-import keras
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, TimeDistributed, LSTM, Dropout, Activation
-from keras.layers import Conv1D, MaxPooling1D, Flatten, Conv2D, BatchNormalization, Lambda
-from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau
-from keras import backend
-from keras.utils import np_utils
-from keras.optimizers import Adam, RMSprop
-from keras import regularizers
 
 
 def pca_plot_2D(tracks, features, genre1, genre2, save=False, filename=None):
@@ -140,87 +131,3 @@ def unison_shuffled_copies(a, b):
     assert len(a) == len(b)
     p = np.random.permutation(len(a))
     return a[p], b[p]
-
-N_LAYERS = 3
-FILTER_LENGTH = 5
-CONV_FILTER_COUNT = 56
-BATCH_SIZE = 32
-LSTM_COUNT = 96
-EPOCH_COUNT = 70
-NUM_HIDDEN = 64
-L2_regularization = 0.001
-
-def conv_recurrent_model_build(model_input):
-    print('Building model...')
-    layer = model_input
-    
-    # 3 1D Conv Layers
-    for i in range(N_LAYERS):
-        layer = Conv1D(
-                filters=CONV_FILTER_COUNT,
-                kernel_size=FILTER_LENGTH,
-                kernel_regularizer=regularizers.l2(L2_regularization),
-                name='convolution_' + str(i + 1)
-            )(layer)
-        layer = BatchNormalization(momentum=0.9)(layer)
-        layer = Activation('relu')(layer)
-        layer = MaxPooling1D(2)(layer)
-        layer = Dropout(0.4)(layer)
-    
-    # LSTM Layer
-    layer = LSTM(LSTM_COUNT, return_sequences=False)(layer)
-    layer = Dropout(0.4)(layer)
-    
-    # Dense Layer
-    layer = Dense(NUM_HIDDEN, kernel_regularizer=regularizers.l2(L2_regularization), name='dense1')(layer)
-    layer = Dropout(0.4)(layer)
-    
-    # Softmax Out
-    layer = Dense(num_classes)(layer)
-    layer = Activation('softmax', name='output_realtime')(layer)
-    model_output = layer
-    model = Model(model_input, model_output)
-    
-    
-    opt = Adam(lr=0.001)
-    model.compile(
-            loss='categorical_crossentropy',
-            optimizer=opt,
-            metrics=['accuracy']
-        )
-    
-    print(model.summary())
-    return model
-
-
-def train_model(x_train, y_train, x_val, y_val):
-    
-    n_features = x_train.shape[2]
-    input_shape = (None, n_features)
-    model_input = Input(input_shape, name='input')
-    model = conv_recurrent_model_build(model_input)
-    
-    print('Training...')
-    history = model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCH_COUNT,
-                        validation_data=(x_val, y_val), verbose=1)
-
-    return model, history
-
-def show_summary_stats(history):
-    print(history.history.keys())
-
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
